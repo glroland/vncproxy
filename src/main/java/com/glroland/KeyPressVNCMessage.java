3de3a75facc6dff358ed772dabf5f36d0e653998
Lee Roland <glroland@hotmail.com>
2025-02-27 12:03:45 -0500

    fixed exception, saved key stroke value keys as a reference

--- src/main/java/com/glroland/KeyPressVNCMessage.java
+++ src/main/java/com/glroland/KeyPressVNCMessage.java
@@ -1,5 +1,38 @@
 package com.glroland;
 
+// +-----------------+--------------------+
+// | Key name        | Keysym value (hex) |
+// +-----------------+--------------------+
+// | BackSpace       | 0xff08             |
+// | Tab             | 0xff09             |
+// | Return or Enter | 0xff0d             |
+// | Escape          | 0xff1b             |
+// | Insert          | 0xff63             |
+// | Delete          | 0xffff             |
+// | Home            | 0xff50             |
+// | End             | 0xff57             |
+// | Page Up         | 0xff55             |
+// | Page Down       | 0xff56             |
+// | Left            | 0xff51             |
+// | Up              | 0xff52             |
+// | Right           | 0xff53             |
+// | Down            | 0xff54             |
+// | F1              | 0xffbe             |
+// | F2              | 0xffbf             |
+// | F3              | 0xffc0             |
+// | F4              | 0xffc1             |
+// | ...             | ...                |
+// | F12             | 0xffc9             |
+// | Shift (left)    | 0xffe1             |
+// | Shift (right)   | 0xffe2             |
+// | Control (left)  | 0xffe3             |
+// | Control (right) | 0xffe4             |
+// | Meta (left)     | 0xffe7             |
+// | Meta (right)    | 0xffe8             |
+// | Alt (left)      | 0xffe9             |
+// | Alt (right)     | 0xffea             |
+// +-----------------+--------------------+
+
 public class KeyPressVNCMessage extends VNCMessage
 {
     public int key = -1;
--- src/main/java/com/glroland/VNCMessageFactory.java
+++ src/main/java/com/glroland/VNCMessageFactory.java
@@ -25,10 +25,14 @@ public class VNCMessageFactory
                 else if (buffer[i] == 4)
                 {
                     byte [] key_buffer = new byte[7];
-                    System.arraycopy(buffer, i+1, key_buffer, 0, key_buffer.length);
+                    int length = key_buffer.length;
+                    if ((i + 1 + length ) <= buffer.length)
+                    {
+                        System.arraycopy(buffer, i+1, key_buffer, 0, length);
 
-                    KeyPressVNCMessage message = new KeyPressVNCMessage(key_buffer);
-                    messages.add(message);
+                        KeyPressVNCMessage message = new KeyPressVNCMessage(key_buffer);
+                        messages.add(message);
+                    }
     
                     i += 7;
                 }
